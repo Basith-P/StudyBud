66404ed1638954ce641b18c20cbf78be230b82b2
Basith P <basithpofficial@gmail.com>
2023-08-30 14:32:23 +0530

    feat: delete room

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('room/<str:id>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:id>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -42,3 +42,14 @@ def updateRoom(request, id):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'obj': room}
+    return render(request, 'base/delete.html', context)
