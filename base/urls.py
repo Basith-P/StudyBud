e720ac442b60eb38bc5ae3d8baad2ccd1b07952d
Basith P <basithpofficial@gmail.com>
2023-08-30 17:18:32 +0530

    feat: add message in a room

--- base/urls.py
+++ base/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', views.registerView, name='register'),
 
     path('', views.home, name='home'),
-    path('room/<str:id>/', views.room, name='room'),
+    path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:id>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
--- base/views.py
+++ base/views.py
@@ -74,9 +74,19 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 
-def room(request, id):
-    room = Room.objects.get(id=id)
-    context = {'room': room}
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
 
